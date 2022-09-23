8f49c517108f50eacaec338c64b042e52e9ced5a
SSEMWOGERERE948 <108024369+SSEMWOGERERE948@users.noreply.github.com>
2022-09-23 11:00:18 +0300

    Add files via upload

---
+++ file.sql
@@ -0,0 +1,2 @@
+INSERT INTO PARTICIPANTS (name, password, product, DOB) VALUES ('name', 'password', 'sfjsdjkldf', '12-12-1222');
+INSERT INTO PRODUCTS (ProductName, description, participantID) VALUES ('jsdfjksdfjksdf', ' jsfjsdfhsdjhd', 12);
